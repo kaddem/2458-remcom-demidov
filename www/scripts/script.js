d6de96312d681aed81b92b5d93de7d69ce013fc8
Kirill Demidov <kademidov@gmail.com>
2021-12-07 14:23:52 +0300

    стилизация чекбокса

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,52 @@
-console.log('script work!');
+let actor;
+
+console.log(actor);
+
+actor = 'Майкл Киттон';
+
+console.log(actor);
+
+actor = 34;
+
+console.log(actor);
+
+const Pi = 3.14;
+
+console.log(Pi);
+
+let petArray = [];
+
+console.log(petArray.length);
+
+
+let a = 10;
+
+a = a + 1;
+
+console.log(a);
+
+a++;
+
+console.log(a);
+
+a = a - 1;
+
+console.log(a);
+
+a--;
+
+console.log(a);
+
+console.log(2 !== '2');
+
+
+let nameMember = ' ';
+
+console.log(nameMember);
+
+if (petArray.length) {
+  console.log('В массиве есть элементы');
+} else {
+  console.log('Массив пуст');
+}
+
